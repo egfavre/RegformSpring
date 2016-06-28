33dc24df22d21487f858d094d688d9d34bc483fa
Liz Favre <egfavre@gmail.com>
2016-06-28 10:28:39 -0400

    add tests

--- src/test/java/com/egfavre/RegformSpringApplicationTests.java
+++ src/test/java/com/egfavre/RegformSpringApplicationTests.java
@@ -1,18 +1,112 @@
 package com.egfavre;
 
+import com.egfavre.entitites.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = RegformSpringApplication.class)
 @WebAppConfiguration
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class RegformSpringApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	@Autowired
+	WebApplicationContext wac;
+
+	MockMvc mockMvc;
+
+	@Before
+	public void before(){
+		mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+		//users.deleteAll();
 	}
+
+
+	@Autowired
+	UserRepository users;
+
+	@Test
+	public void atestAddUser() throws Exception {
+		User user = new User ();
+		user.setUsername("alice");
+		user.setAddress("alkdjfs");
+		user.setEmail("alkdsfj@lakdjfs.com");
+
+		ObjectMapper om = new ObjectMapper();
+		String json = om.writeValueAsString(user);
+
+		mockMvc.perform(
+				MockMvcRequestBuilders.post("/user")
+						.content(json)
+						.contentType("application/json")
+		);
+
+		Assert.assertTrue(users.count() == 1);
+	}
+
+	@Test
+	public void bTestEdit() throws Exception {
+		User user = users.findOne(1);
+		user.setUsername("Bob");
+
+		ObjectMapper om = new ObjectMapper();
+		String json = om.writeValueAsString(user);
+
+		mockMvc.perform(
+				MockMvcRequestBuilders.post("/user")
+						.content(json)
+						.contentType("application/json")
+		);
+
+		User editted = users.findOne(1);
+
+		Assert.assertTrue(editted.getUsername().equals("Bob"));
+	}
+
+	@Test
+	public void ctestGet() throws Exception {
+		ResultActions ra = mockMvc.perform(
+				MockMvcRequestBuilders.get("/user")
+		);
+		MvcResult result= ra.andReturn();
+		MockHttpServletResponse response = result.getResponse();
+		String json = response.getContentAsString();
+
+		ObjectMapper om = new ObjectMapper();
+		ArrayList<HashMap<String, String>> userMaps = om.readValue(json, ArrayList.class);
+
+		Assert.assertTrue(userMaps.size() == 1);
+	}
+
+	@Test
+	public void dtestDeleteUser() throws Exception {
+		mockMvc.perform(
+				MockMvcRequestBuilders.delete("/user/1")
+		);
+
+		Assert.assertTrue(users.count() == 0);
+	}
+
 
 }
